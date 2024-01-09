455ea178b8e95d0f1907e4e0d548cd7997f655c6
ryu <114303361+ryuapp@users.noreply.github.com>
2024-01-09 23:15:52 +0900

    Add static dir for docs

--- docs/server.ts
+++ docs/server.ts
@@ -1,11 +1,11 @@
 import { render } from "https://deno.land/x/gfm@0.3.0/mod.ts";
 import "https://esm.sh/prismjs@1.29.0/components/prism-typescript?no-check";
+import { serveDir } from "https://deno.land/std@0.211.0/http/file_server.ts";
 import { Router } from "../src/mod.ts";
 
 const title = "Baku - A router for Deno";
 const description = "Baku is a router for Deno";
 
-const css = await Deno.readTextFile("docs/styles/markdown.min.css");
 const markdown = await Deno.readTextFile("docs/pages/index.md");
 const body = render(markdown);
 
@@ -17,7 +17,7 @@ const html = `
     <title>${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="${description}">
-    <style>${css}</style>
+    <link href="markdown.min.css" rel="stylesheet" />
   </head>
   <body>
     <main class="markdown-body" style="max-width: 768px;margin: 0 auto;">
@@ -28,7 +28,10 @@ const html = `
 `;
 
 const router = Router();
+
 router.get("/", () => {
   return new Response(html, { headers: { "content-type": "text/html" } });
 });
+router.get("*", (req) => serveDir(req, { fsRoot: "docs/static", quiet: true }));
+
 Deno.serve(router.handle);
